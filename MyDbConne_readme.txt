85f405d6fdb6eb316b6599bc4d302b406a7eeadf
Jozsef Kiss <info@kisscodesystems.com>
2017-04-01 14:04:57 +0200

    Add files via upload

---
+++ MyDbConne_readme.txt
@@ -0,0 +1,19 @@
+Please download the source and compile it or download the jar from
+https://github.com/kisscodesystems/MyDbConne/blob/master/MyDbConne.jar
+MD5 hash of file MyDbConne.jar: (v1.0)
+f8 33 d1 52 e7 35 4c fb 55 fb 2e ed ec cc be 7d
+SHA1 hash of file MyDbConne.jar: (v1.0)
+96 7f 2b 95 00 1f be 68 23 4b 03 6e 04 e7 8f f1 d6 18 02 42
+SHA512 hash of file MyDbConne.jar: (v1.0)
+f2 55 a9 e1 4a 78 10 c0 14 22 25 8f 24 9d 77 cb ac c0 12 58 aa eb 3e 91 68 61 35 37 73 86 9d 91 39 dd bc 31 56 7b 75 58 d2 35 00 d9 c7 92 8f a1 1d 90 bf 52 1e 90 1c 59 65 e4 55 b4 8c ce bc 11
+Please move MyDbConne.jar into a safe folder.
+Open a command line and navigate into that folder.
+Type "java -version" to see the actual version of your java. (min 1.8 is required.)
+See that the "java" command points to the correct java executable!
+Example command to start this application:
+java -cp C:\drivers\OracleJdbc.jar;C:\opensourcejava\MyDbConne\MyDbConne.jar com.kisscodesystems.MyDbConne.Main
+(Please change the folder of the database driver and the place of the MyDbConne.jar into your folders.)
+Type "java -jar MyDbConne.jar ?" for the usable commands.
+Type "java -jar MyDbConne.jar help" for more detailed information.
+Type "java -jar MyDbConne.jar application story" if you want to read.
+Type "java -jar MyDbConne.jar application describe" if you would like to get more details.
---
+++ src/com/kisscodesystems/MyDbConne/Main.java
@@ -0,0 +1,102 @@
+/*
+** MyDbConne application.
+**
+** Description:    : This tiny application works for you as a command line
+**                   sql client. Supports Oracle, Mssql, Db2 and Postgresql.
+**                   Handles 42 commands and this is not an accident.
+**
+** Published       : 04.01.2017
+**
+** Current version : 1.0
+**
+** Developed by    : Jozsef Kiss
+**                   KissCode Systems Kft
+**                   <http://www.prdare.com>
+**
+** Changelog       : 1.0 - 04.01.2017
+**                   Initial release.
+**
+** Copyright (C) 2017 KissCode Systems Kft
+**
+** Example command to start this application:
+**   "C:\Program Files\Java\jdk1.8.0_121\bin\java.exe" /
+**   -cp C:\drivers\OracleJdbc.jar;C:\opensourcejava\MyDbConne\MyDbConne.jar /
+**   com.kisscodesystems.MyDbConne.Main
+**
+** Class hierarchy:
+**   - Utils
+**     Independent utilities.
+**   - Const -> Base -> Conn -> Qdata -> Print -> Upper -> Exec -> Queries
+**     The Queries class is the thread to manage this application.
+**   - Query
+**     This will be the thread of each separately started queries.
+**   - Main
+**     This will start the application.
+**
+** Queries and Query implement runnable (these are threads!)
+** and the others don't.
+**
+** MyDbConne is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** Free Software Foundation, version 3.
+**
+** MyDbConne is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with MyDbConne. If not, see <http://www.gnu.org/licenses/>.
+*/
+package com . kisscodesystems . MyDbConne ;
+import java . io . IOException ;
+import java . io . ObjectInputStream ;
+import java . io . ObjectOutputStream ;
+public final class Main
+{
+// This is to be called when the user starts the application.
+  public static void main ( String [ ] args )
+  {
+// Let's create the object that will be the main thread instead of this.
+    Queries queries = new Queries ( ) ;
+// The arguments will be its parameter.
+    queries . setArgs ( args ) ;
+// Let's create the thread of this object and start it!
+    Thread queriesThread = new Thread ( queries ) ;
+    if ( queriesThread != null )
+    {
+      queriesThread . start ( ) ;
+// Waiting for this thread to be joined.
+      try
+      {
+        queriesThread . join ( ) ;
+      }
+      catch ( InterruptedException e )
+      {
+        System . out . println ( "Exception while joining queriesThread: " + e . toString ( ) ) ;
+      }
+      finally
+      {
+// This can be null now.
+        queriesThread = null ;
+      }
+    }
+    else
+    {
+      System . out . println ( "The queriesThread of Queries is found as null." ) ;
+    }
+// Just for fun.
+    System . exit ( 0 ) ;
+  }
+// These are for prevent serialize-deserialize this object.
+  private final void readObject ( ObjectInputStream in )
+    throws IOException
+  {
+    throw new IOException ( "" ) ;
+  }
+  private final void writeObject ( ObjectOutputStream out )
+    throws IOException
+  {
+    throw new IOException ( "" ) ;
+  }
+}
